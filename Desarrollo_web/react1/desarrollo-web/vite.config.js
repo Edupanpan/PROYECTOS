898f56bfb2cf25ca3b411db55ce95027e57e3d64
edupanpan <eduardo20032110@gmail.com>
2024-10-09 12:17:00 -0300

    asdkjahsjd

--- Desarrollo_web/react1/desarrollo-web/vite.config.js
+++ Desarrollo_web/react1/desarrollo-web/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PROYECTOS/Desarrollo_web/react1/desarrollo-web/', // Ruta base para GitHub Pages
   plugins: [react()],
-})
+  build: {
+    outDir: 'build', // Directorio de salida
+  },
+});
